34b44c56448c68e1df567b12ff30b0f1a1307295
dejanae Mcneal <dejanae.mcneal@my.southeastmn.edu>
2018-09-12 18:10:36 -0500

    Completed exercise 1

---
+++ mswanson1b2/mswanson1b2/frmInvoiceTotal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mswanson1b2
+{
+    public partial class dmcneal1b1 : Form
+    {
+        public dmcneal1b1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txtDiscountAmount.Text = 
+                (Convert.ToDecimal(txtSubtotal.Text)
+                * Convert.ToDecimal(txtDiscountPercent.Text) / 100).ToString("0.00");
+            txtTotal.Text =
+                (Convert.ToDecimal(txtSubtotal.Text)
+                - Convert.ToDecimal(txtDiscountAmount.Text)).ToString("0.00");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtDiscountAmount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
